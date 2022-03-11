4e622455a64d342e13516194f89cbc118cdd35ba
faisalkn-71 <csust2021@gmail.com>
2022-03-11 17:50:57 +0600

    final-commit

--- js/app.js
+++ js/app.js
@@ -40,7 +40,6 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById("qna-part").style.display = "none";
-
         document.getElementById( "reported" ).style.display = "none";
 
         displayLikedPosts();
