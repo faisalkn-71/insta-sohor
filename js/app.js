f28cb7707fc900bcee37443cb8a4b6853262823e
faisalkn-71 <csust2021@gmail.com>
2022-03-11 10:57:16 +0600

    reported-post-done

--- js/app.js
+++ js/app.js
@@ -152,7 +152,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+   
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
